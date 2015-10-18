08bbe6647d4dc97919abd102f9488c4bd9f1e0a6
Bo Lopker <blopker@23andme.com>
2015-10-17 20:02:04 -0700

    2 done

---
+++ elixir/2.exs
@@ -0,0 +1,9 @@
+require Integer
+
+defmodule Fib do
+	def fib(n) do fib(1, 1, n) end
+	def fib(a, _, 0) do a end
+	def fib(a, b, n) do	fib(b, a + b, n-1) end
+end
+
+1..32 |> Stream.map(&Fib.fib/1) |> Stream.filter(Integer.is_even/1) |> Enum.sum |> IO.puts
